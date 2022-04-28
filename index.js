49243089f1a42232bdb669dbf06073406b47dcfc
AR31313 <gurlgotgame@gmail.com>
2022-04-27 22:02:58 -0700

    js done

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-// const Engineer = require("./lib/engineer");
-// const Intern = require("./lib/intern");
-// const Manager = require("./lib/manager");
-// const Employee = require("./lib/employee");
+const manager = require("./lib/manager");
+const engineer = require("./lib/engineer");
+const intern = require("./lib/intern");
+const Engineer = require("./src/engineerCard");
+const Intern = require("./src/employeeCard");
+const Manager = require("./src/internCard");
+const Employee = require("./src/managerCard");
+
 const inquirer = require('inquirer');
-const fs = require("fs");
 var employeeArray = [];
 
-let managers = [];
-let engineers = [];
-let interns = [];
+// let managers = [];
+// let engineers = [];
+// let interns = [];
 
 function initPromptLoop() {
     inquirer
@@ -22,140 +25,151 @@ function initPromptLoop() {
                 const employeeRole = choice.position;
                 if (employeeRole === "Manager") {
                     initManagerQuestions();
+                } else if (employeeRole === "Engineer") {
+                    initEngineerQuestions();
+                } else if (employeeRole === "Intern") {
+                    initInternQuestions();
                 }
             })
+};
+
+function initManagerQuestions() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "charName",
+                message: "What is your Name?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please enter your email: "
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your id#? "
+            },
+            {
+                type: "input",
+                name: "officeNum",
+                message: "What is your Office Number? "
+            }
+        ])
+        .then(function (response) {
+            // creates a new Manager Object with responses, which adds the response to the employeeArray.
+            const newManager = new manager(
+                response.charName,
+                response.id,
+                response.email,
+                response.officeNum,
+            );
+            employeeArray.push(newManager);
+            addEmployee();
+        });
 }
 
-initManagerQuestions() {
-    // ask manager questions, then push data to managers array
+function initEngineerQuestions() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "charName",
+                message: "What is your Name?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please enter your email: "
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your id#? "
+            },
+            {
+                type: "input",
+                name: "gitHub",
+                message: "What is your GitHub Username? "
+            }
+        ]).then(function (response) {
+            // creates a new Manager Object with responses, which adds the response to the employeeArray.
+            const newEngineer = new engineer(
+                response.charName,
+                response.id,
+                response.email,
+                response.gitHub,
+            );
+            employeeArray.push(newEngineer);
+            addEmployee();
+        });
 
 };
 
-initEngineerQuestions() {
-    // ask manager questions, then push data to engineers array
+function initInternQuestions() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "charName",
+                message: "What is your Name?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please enter your email: "
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your id#? "
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "Name of your school? "
+            }
+        ]).then(function (response) {
+            // creates a new Manager Object with responses, which adds the response to the employeeArray.
+            const newInterns = new intern(
+                response.charName,
+                response.id,
+                response.email,
+                response.school,
+            );
+            employeeArray.push(newInterns);
+            addEmployee();
+        });
 
 };
 
-initInternQuestions() {
-    // ask manager questions, then push data to interns array
+// ask if they have another employee
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Add another Employee?',
+                name: 'secondEmployee',
+                choices: ['Yes', 'No'],
+            },
+        ]).then(function (choiceTwo) {
+            // if yes, start at the beginning again
+            if (choiceTwo.secondEmployee === 'Yes') {
+                initPromptLoop();
+                // if no, Generate the HTML from the saved employees
+            } else {
+                generateHTML(employeeArray);
+            }
+        });
+}
+function generateHTML(employeeArray) {
+    const Engineer = require("./src/engineerCard");
+    const Intern = require("./src/employeeCard");
+    const Manager = require("./src/internCard");
+    const Employee = require("./src/managerCard");
+}
 
-};
-
-// function prompt() {
-//     inquirer
-//         .prompt(
-//             {
-//                 type: 'list',
-//                 message: 'What is your Title?',
-//                 name: 'position',
-//                 choices: ['Manager', 'Engineer', 'Intern'],
-//             }).then((choice) => {
-//                 const employeeRole = choice.position
-//                 if (employeeRole == 'Manager') {
-//                     initManagerQuestions();
-//                     inquirer
-//                         .prompt([
-//                             {
-//                                 type: "input",
-//                                 name: "charName",
-//                                 message: "What is your Name?",
-//                             },
-//                             {
-//                                 type: "input",
-//                                 name: "email",
-//                                 message: "Please enter your email: "
-//                             },
-//                             {
-//                                 type: "input",
-//                                 name: "id",
-//                                 message: "What is your id#? "
-//                             },
-//                             {
-//                                 type: "input",
-//                                 name: "officeNum",
-//                                 message: "What is your Office Number? "
-//                             }
-//                         ])
-
-//                 } else if (employeeRole == 'Engineer') {
-//                     inquirer
-//                         .prompt([
-//                             {
-//                                 type: "input",
-//                                 name: "charName",
-//                                 message: "What is your Name?",
-//                             },
-//                             {
-//                                 type: "input",
-//                                 name: "email",
-//                                 message: "Please enter your email: "
-//                             },
-//                             {
-//                                 type: "input",
-//                                 name: "id",
-//                                 message: "What is your id#? "
-//                             },
-//                             {
-//                                 type: "input",
-//                                 name: "gitHub",
-//                                 message: "What is your GitHub Username? "
-//                             }
-//                         ])
-//                 } else if (employeeRole == 'Intern') {
-//                     inquirer
-//                         .prompt([
-//                             {
-//                                 type: "input",
-//                                 name: "charName",
-//                                 message: "What is your Name?",
-//                             },
-//                             {
-//                                 type: "input",
-//                                 name: "email",
-//                                 message: "Please enter your email: "
-//                             },
-//                             {
-//                                 type: "input",
-//                                 name: "id",
-//                                 message: "What is your id#? "
-//                             },
-//                             {
-//                                 type: "input",
-//                                 name: "school",
-//                                 message: "Name of your school? "
-//                             }
-//                         ]) // ask if they have another employee
-//                     inquirer
-//                         .prompt([
-//                             {
-//                                 type: 'list',
-//                                 message: 'Do you want to add another Employee?',
-//                                 name: 'addEmployee',
-//                                 choices: ['Yes', 'No'],
-//                             }
-//                         ])
-//                         .then((choiceTwo) => {
-//                             employeeRole = choiceTwo.addEmployee
-//                             if (employeeRole == 'Yes') {
-//                                 prompt()
-//                             } else {
-//                                 console.log('what else?')
-//                             }
-//                         })
-
-//                         .then((data) => {
-//                             const filename = `./dist/README.md`;
-
-//                             fs.writeFile(filename, string, (err) =>
-//                                 err ? console.log(err) : console.log('Success!')
-//                             );
-//                         });
-//                 }
-
-                    // ask if they have another employee
-
-                    // .then((choiceTwo) => {
-                    //     const employeeRole = choiceTwo.position
-                    //     if (employeeRole == 'Manager') {
-                    // if yes, start at the beginning again
-                    // if no, Generate the HTML from the saved employees
+// call the initPromptLoop function to start the Prompt Questions
+initPromptLoop();
--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -2,23 +2,21 @@ const Employee = require("../lib/employee")
 
 test("grab the name of employee", function () {
     let carl = new Employee("Carl Weezer", 19, "carl@gmail.com")
-    // console.table(carl)
-    expect(carl.name == "Carl Weezer")
+    expect(carl.name).toBe("Carl Weezer")
 })
 
 test("grab the ID of employee", function () {
     let carl = new Employee("Carl Weezer", 19, "carl@gmail.com")
-    // console.table(carl)
+    console.table(carl)
     expect(carl.id).toBe(19)
 })
 test("grab the email of employee", function () {
     let carl = new Employee("Carl Weezer", 19, "carl@gmail.com")
-    // console.log(carl.printInfo())
     expect(carl.email).toBe("carl@gmail.com")
 })
 
 test("getName return", function () {
     let carl = new Employee("Carl Weezer", 19, "carl@gmail.com")
-    // console.log(carl.printInfo())
+    console.log(carl.printInfo())
     expect(carl.getName() == "Carl Weezer")
 })
